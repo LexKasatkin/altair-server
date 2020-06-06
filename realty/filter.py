f27c6eafbb2c706bee8aec4e9fac02469278a5d4
Kasatkin <aleksey.kasatkin@saritasa.com>
2020-06-06 13:50:50 +0700

    Add range filters for cost and square

--- realty/filter.py
+++ realty/filter.py
@@ -2,7 +2,9 @@ from .models import Flat
 from django_filters import rest_framework as filters
 
 class FlatFilter(filters.FilterSet):
+    cost = filters.RangeFilter()
+    square = filters.RangeFilter()
     class Meta:
         model = Flat
         fields = ['id', 'city', 'wall_material', 'flat_type',
-                  'developer', 'district']
+                  'developer', 'district', 'cost', 'square']
