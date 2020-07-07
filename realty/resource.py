057b60da7d3a796623bb6cbd35f2c2b9c1bb5274
Kasatkin <aleksey.kasatkin@saritasa.com>
2020-07-07 11:34:45 +0300

    Change of export fields in flat resource

--- realty/resource.py
+++ realty/resource.py
@@ -7,10 +7,13 @@ class CityResource(resources.ModelResource):
         model = City
 
 class FlatResource(resources.ModelResource):
-
     class Meta:
         model = Flat
-        fields = ('street', 'subject_of_law', 'description', 'wall_material',
-                'flat_type','developer','cost','square')
-        export_order = ('street', 'flat_type', 'square', 'subject_of_law',
-                  'wall_material','cost','developer','description')
+        fields = ('house__residential_complex__name', 'house__street__name', 'house__house', 'house__street__district__name',
+                  'flat_type__name', 'floor', 'house__max_floor', 'square', 'subject_of_law',
+                  'house__wall_material__name', 'cost', 'house__developer__name', 'house__year_of_completion', 'house__quarter',
+                  'description',)
+        export_order = ('house__residential_complex__name', 'house__street__name', 'house__house', 'house__street__district__name',
+                        'flat_type__name', 'floor', 'house__max_floor', 'square', 'subject_of_law',
+                  'house__wall_material__name', 'cost', 'house__developer__name', 'house__year_of_completion', 'house__quarter',
+                  'description',)
